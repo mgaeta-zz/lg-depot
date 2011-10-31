aa8a266b38334e60a7dfcdbebbd2d925e3b5992c
Marcos Gaeta <mgaeta89@gmail.com>
2011-10-31 04:03:22 -0700

    Innit Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LgDepot::Application.config.secret_token = 'd169717163239cf9e979b23d314a699c63ebe7c466f06ff88ce9a8d78b1baf3e13b5cea904d038d78954637a78888b81913dd613eaded1a8cb5975b1400fbdfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LgDepot::Application.config.session_store :cookie_store, key: '_lg-depot_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LgDepot::Application.config.session_store :active_record_store
