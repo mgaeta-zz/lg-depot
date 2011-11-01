95f03b32d29c26f9f37b5946aaf7698b3270e0dd
Marcos Gaeta <mgaeta89@gmail.com>
2011-11-01 16:54:20 -0700

    unit testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(:title => "Shit Poo Poo", 
+  :description =>
+    %{<p>
+      THis is the description it is mad cool
+      Dick butt poo poo oh yeah.
+     </p>},
+  :image_url => "/images/poop1.jpg",
+  :price => 49.50)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
+require 'minitest/autorun'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
