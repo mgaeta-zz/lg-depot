03eea81634cc148d815b98515c6a3cc9dfd373f1
Marcos Gaeta <mgaeta89@gmail.com>
2011-11-02 15:24:03 -0700

    chapter 9

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -9,4 +11,14 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
     assert product.errors[:price].any?
   end
+
+  test "product is not valis without a unique title" do
+    product = Product.new(:title       => products(:ruby).title,
+                          :description => "yyy",
+                          :price       => 1,
+                          :image_url   => "fred.gif")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join("; ")
+  end
+
 end
